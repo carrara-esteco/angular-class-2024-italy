6623b8b9f6761b668cba1533e24e422b638b76fb
Alessandro Carrara <carrara@esteco.com>
2024-11-19 08:46:08 +0100

    Prep for lesson 2

---
+++ html-css/lesson2/compileScss.js
@@ -0,0 +1,27 @@
+const sass = require('sass');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const fs = require('fs');
+
+function buildCSS() {
+    const result = sass.compile('styles.scss',
+        {
+            loadPaths: ["node_modules/"],
+            outputStyle: "compressed"
+        });
+
+    return postCss(result.css);
+}
+
+function postCss(css) {
+    return postcss([autoprefixer])
+        .process(css, {from: undefined})
+        .then((result) => {
+            result.warnings().forEach((warn) => {
+                console.warn(warn.toString());
+            });
+            fs.writeFile('styles.css', result.css, () => console.log('Compiled styles.css'));
+        });
+}
+
+buildCSS();
